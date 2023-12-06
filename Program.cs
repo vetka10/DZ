28293a458c05ac6115d32c4d14f11cf259ec9af0
Екатерина <melfon@bk.ru>
2023-12-06 20:38:01 +0300

    Задача 64/66/68

--- Program.cs
+++ Program.cs
@@ -1,42 +1,49 @@
-﻿//Console.Write("Введи N: ");
-//int N = Convert.ToInt16(Console.ReadLine());
-//Console.Write("Введи M: ");
-//int M = Convert.ToInt16(Console.ReadLine());
-//Console.WriteLine($"N={N}, M={M}");
-//if (M>N)
-//for (int i = N; i <= M; i++)
-   // Console.Write($" {i}");
-//else
-    //for (int i = M; i <= N; i++)
-        //Console.Write($" {i}");
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 6, 7, 8"
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
 
-        //Console.Write("Введите число M: ");
-//int m = Convert.ToInt32(Console.ReadLine());
+       //Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30 
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-//Console.Write("Введите число N: ");
-//int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-//SumFromMToN(m, n);
+SumFromMToN(m, n);
 
-// вызов функции "сумма чисел от M до N"
-//void SumFromMToN(int m, int n)
-//{
-// Console.Write(SumMN(m - 1, n));
-//}
+void SumFromMToN(int m, int n)
+{
+ Console.Write(SumMN(m - 1, n));
+}
 
-// функция сумма чисел от M до N
-//int SumMN(int m, int n)
-//{
-  //  int res = m;
-    //if (m == n)
-     //   return 0;
-    //else
-    //{
-       // m++;
-       // res = m + SumMN(m, n);
-      //  return res;
-    //}
-//}
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 29
 
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
